e001ff740ed2827f80c8d99831568219a14972c9
claudiosmweb <contato@claudiosmweb.com>
2014-04-07 22:26:30 -0300

    created a initial main plugin file

---
+++ woocommerce-rache-aqui.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Rache Aqui!
+ * Plugin URI: https://www.racheaqui.com.br/
+ * Description: Integre o Rache Aqui! com o seu WooCommerce
+ * Author: claudiosanches, Rache Aqui!
+ * Author URI: http://claudiosmweb.com/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-rache-aqui
+ * Domain Path: /languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! class_exists( 'WC_Rache_Aqui' ) ) :
+
+/**
+ * WooCommerce Rache Aqui! main class.
+ */
+class WC_Rache_Aqui {
+
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin public actions.
+	 */
+	private function __construct() {
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+		// Checks with WooCommerce is installed.
+		if ( class_exists( 'WC_Payment_Gateway' ) ) {
+			// Include the WC_Rache_Aqui_Gateway class.
+			include_once 'includes/class-wc-rache-aqui-gateway.php';
+
+			add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateway' ) );
+		} else {
+			add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+		}
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		$domain = 'woocommerce-rache-aqui';
+		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+		load_plugin_textdomain( $domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Add the gateway to WooCommerce.
+	 *
+	 * @param   array $methods WooCommerce payment methods.
+	 *
+	 * @return  array          Payment methods with Rache Aqui!.
+	 */
+	public function add_gateway( $methods ) {
+		$methods[] = 'WC_Rache_Aqui_Gateway';
+
+		return $methods;
+	}
+
+	/**
+	 * WooCommerce fallback notice.
+	 *
+	 * @return string
+	 */
+	public function woocommerce_missing_notice() {
+		echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Rache Aqui! depends on the last version of %s to work!', 'woocommerce-rache-aqui' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">' . __( 'WooCommerce', 'woocommerce-rache-aqui' ) . '</a>' ) . '</p></div>';
+	}
+}
+
+add_action( 'plugins_loaded', array( 'WC_Rache_Aqui', 'get_instance' ), 0 );
+
+endif;
