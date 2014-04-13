cc3e42e1afebb734b41809c423e0897e09fc79c7
claudiosmweb <contato@claudiosmweb.com>
2014-04-13 01:01:03 -0300

    updated the plugin version to 2.0.0

--- woocommerce-racheaqui.php
+++ woocommerce-racheaqui.php
@@ -5,7 +5,7 @@
  * Description: Integre o Rache Aqui! com o seu WooCommerce
  * Author: claudiosanches, Rache Aqui!
  * Author URI: http://claudiosmweb.com/
- * Version: 1.0.0
+ * Version: 2.0.0
  * License: GPLv2 or later
  * Text Domain: woocommerce-racheaqui
  * Domain Path: /languages/
@@ -27,7 +27,7 @@ class WC_RacheAqui {
 	 *
 	 * @var string
 	 */
-	const VERSION = '1.0.0';
+	const VERSION = '2.0.0';
 
 	/**
 	 * Instance of this class.
