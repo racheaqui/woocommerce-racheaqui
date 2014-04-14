6792590bf86e3a8a53d3f78c29b9df12766e06f2
claudiosmweb <contato@claudiosmweb.com>
2014-04-14 11:35:03 -0300

    created the payment form

--- includes/class-wc-racheaqui-gateway.php
+++ includes/class-wc-racheaqui-gateway.php
@@ -43,6 +43,8 @@ class WC_RacheAqui_Gateway extends WC_Payment_Gateway {
 		// Actions.
 		// add_action( 'woocommerce_api_wc_racheaqui_gateway', array( $this, 'check_ipn_response' ) );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		// add_action( 'woocommerce_receipt_' .  $this->id, array( $this, 'receipt_page' ) );
+		add_action( 'woocommerce_receipt_racheaqui', array( $this, 'receipt_page' ) );
 
 		// Active logs.
 		if ( 'yes' == $this->debug ) {
@@ -206,6 +208,29 @@ class WC_RacheAqui_Gateway extends WC_Payment_Gateway {
 	}
 
 	/**
+	 * Generate the args to form.
+	 *
+	 * @param  WC_Order $order Order data.
+	 *
+	 * @return array           Form arguments.
+	 */
+	public function get_form_args( $order ) {
+		// $this->sandbox
+		// $this->force_error
+		$args = array(
+			'lojaID'          => $this->store_id,
+			'valor_pedido'    => number_format( $order->order_total, 2, '', '' ),
+			'pedidoID'        => $this->invoice_prefix . $order->id,
+			'num_raches'      => $this->split,
+			'maximo_parcelas' => $this->installments,
+		);
+
+		$args = apply_filters( 'woocommerce_racheaqui_form_args', $args, $order );
+
+		return $args;
+	}
+
+	/**
 	 * Generate the form.
 	 *
 	 * @param int     $order_id Order ID.
@@ -213,7 +238,73 @@ class WC_RacheAqui_Gateway extends WC_Payment_Gateway {
 	 * @return string           Payment form.
 	 */
 	public function generate_form( $order_id ) {
+		$order      = new WC_Order( $order_id );
+		$args       = $this->get_form_args( $order );
+		$form_args  = array();
 
+		if ( 'yes' == $this->debug ) {
+			$this->log->add( $this->id, 'Payment arguments for order ' . $order->get_order_number() . ': ' . print_r( $args, true ) );
+		}
+
+		foreach ( $args as $key => $value ) {
+			$form_args[] = '<input type="hidden" name="' . esc_attr( $key ) . '" value="' . esc_attr( $value ) . '" />';
+		}
+
+		if ( function_exists( 'wc_enqueue_js' ) ) {
+			wc_enqueue_js( '
+				jQuery.blockUI({
+					message: "' . esc_js( __( 'Thank you for your order. We are now redirecting you to Rache Aqui! to make payment.', 'woocommerce-racheaqui' ) ) . '",
+					baseZ: 99999,
+					overlayCSS: {
+						background: "#fff",
+						opacity: 0.6
+					},
+					css: {
+						padding:        "20px",
+						zindex:         "9999999",
+						textAlign:      "center",
+						color:          "#555",
+						border:         "3px solid #aaa",
+						backgroundColor:"#fff",
+						cursor:         "wait",
+						lineHeight:		"24px",
+					}
+				});
+				jQuery("#submit-payment-form").click();
+			' );
+		} else {
+			$this->woocommerce_instance()->add_inline_js( '
+				jQuery( "body" ).block({
+					message: "' . esc_js( __( 'Thank you for your order. We are now redirecting you to Rache Aqui! to make payment.', 'woocommerce-racheaqui' ) ) . '",
+					overlayCSS: {
+						background: "#fff",
+						opacity:    0.6
+					},
+					css: {
+						padding:         "20px",
+						zIndex:          "9999999",
+						textAlign:       "center",
+						color:           "#555",
+						border:          "3px solid #aaa",
+						backgroundColor: "#fff",
+						cursor:          "wait",
+						lineHeight:      "24px"
+					}
+				});
+				jQuery("#submit-payment-form").click();
+			' );
+		}
+
+		if ( 'yes' == $this->sandbox ) {
+			$url = $this->sandbox_url;
+		} else {
+			$url = $this->production_url;
+		}
+
+		return '<form action="' . $url . '" method="post" id="payment-form" target="_top">
+				' . implode( '', $form_args ) . '
+				<input type="submit" class="button alt" id="submit-payment-form" value="' . __( 'Pay via Rache Aqui!', 'woocommerce-racheaqui' ) . '" /> <a class="button cancel" href="' . esc_url( $order->get_cancel_order_url() ) . '">' . __( 'Cancel order &amp; restore cart', 'woocommerce-racheaqui' ) . '</a>
+			</form>';
 	}
 
 	/**
@@ -245,6 +336,7 @@ class WC_RacheAqui_Gateway extends WC_Payment_Gateway {
 	 * @return void
 	 */
 	public function receipt_page( $order ) {
+		echo '<p>' . __( 'Thank you for your order, please click the button below to pay with Rache Aqui!.', 'woocommerce-racheaqui' ) . '</p>';
 		echo $this->generate_form( $order );
 	}
 
