250f150dffc880ecdc81a0c4d932493a403d78fd
claudiosmweb <contato@claudiosmweb.com>
2014-04-15 02:05:21 -0300

    improved the plugin actions

--- includes/class-wc-racheaqui-gateway.php
+++ includes/class-wc-racheaqui-gateway.php
@@ -44,8 +44,7 @@ class WC_RacheAqui_Gateway extends WC_Payment_Gateway {
 		// Actions.
 		// add_action( 'woocommerce_api_wc_racheaqui_gateway', array( $this, 'check_ipn_response' ) );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-		// add_action( 'woocommerce_receipt_' .  $this->id, array( $this, 'receipt_page' ) );
-		add_action( 'woocommerce_receipt_racheaqui', array( $this, 'receipt_page' ) );
+		add_action( 'woocommerce_receipt_' .  $this->id, array( $this, 'receipt_page' ) );
 
 		// Active logs.
 		if ( 'yes' == $this->debug ) {
